31b6130d74c94d882553b7e59a7643971915d119
Queenen <AnnBjo66962@stud.noroff.no>
2024-02-21 15:27:14 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Home from "./pages/HomePage";
+import Home from "./pages/HomePage/";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/global.css";
 import "./styles/global.scss";
