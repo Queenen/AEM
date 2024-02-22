06168d4ac5d0f41e6350e715aa9d3ee8f9d2c90d
Queenen <AnnBjo66962@stud.noroff.no>
2024-02-22 20:28:01 +0100

    Update index.jsx

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className="p-4 p-lg-5 bg-primary text-light d-flex justify-content-center justify-content-between justify-content-md-evenly">
-      <ul className="list-unstyled small lh-lg m-0">
+    <footer className="p-4 p-lg-5 bg-primary text-light d-flex justify-content-center justify-content-evenly">
+      <ul className="list-unstyled small lh-lg m-0 d-flex flex-column col-4 col-md-2">
         <li disabled className="fw-bold fs-6 dashedUnderline mb-4">
-          Om AEM
+          AEM
         </li>
         <li>
           <Nav.Link as={Link} to="/AEM">
@@ -15,7 +15,7 @@ function Footer() {
           </Nav.Link>
         </li>
       </ul>
-      <ul className="list-unstyled small lh-lg m-0">
+      <ul className="list-unstyled small lh-lg m-0 d-flex flex-column col-4 col-md-2">
         <li disabled className="fw-bold fs-6 dashedUnderline mb-4">
           Få Hjelp
         </li>
@@ -25,18 +25,20 @@ function Footer() {
           </Nav.Link>
         </li>
         <li>
-          <Nav.Link as={Link} to="/brukervilkår" className="mt-3">
+          <Nav.Link as={Link} to="/brukervilkår" className="mt-2">
             Brukervilkår
           </Nav.Link>
         </li>
       </ul>
-      <Nav.Link as={Link} to="/">
-        <img
-          src={`${process.env.PUBLIC_URL}/images/AEM-logo-2.png`}
-          alt="AEM logo"
-          id="logo"
-        />
-      </Nav.Link>
+      <div className="d-none d-sm-block d-flex flex-column col-2">
+        <Nav.Link as={Link} to="/">
+          <img
+            src={`${process.env.PUBLIC_URL}/images/AEM-logo-2.png`}
+            alt="AEM logo"
+            id="logo"
+          />
+        </Nav.Link>
+      </div>
     </footer>
   );
 }
