efbd75fab14c3278a336fbd0f1695251ddca5358
Annette B <114480324+Queenen@users.noreply.github.com>
2024-02-24 12:40:48 +0100

    made changes to footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className="p-4 p-lg-5 bg-primary text-light d-flex justify-content-center justify-content-evenly">
-      <ul className="list-unstyled small lh-lg m-0 d-flex flex-column col-4 col-md-2">
+    <footer className="py-4 py-lg-5 bg-primary text-light d-flex flex-wrap justify-content-center justify-content-evenly">
+      <ul className="list-unstyled small lh-lg m-0 d-flex flex-column col-4 col-sm-2 ps-4 ps-lg-5">
         <li disabled className="fw-bold fs-6 dashedUnderline mb-4">
-          AEM
+          Informasjon
         </li>
         <li>
           <Nav.Link as={Link} to="/AEM">
@@ -15,22 +15,16 @@ function Footer() {
           </Nav.Link>
         </li>
       </ul>
-      <ul className="list-unstyled small lh-lg m-0 d-flex flex-column col-4 col-md-2">
+      <ul className="list-unstyled small lh-lg m-0 d-flex flex-column col-auto pe-4 pe-sm-0">
         <li disabled className="fw-bold fs-6 dashedUnderline mb-4">
-          Få Hjelp
+          Kontakt
         </li>
-        <li>
-          <Nav.Link as={Link} to="/personvern">
-            Personvern & Cookies
-          </Nav.Link>
-        </li>
-        <li>
-          <Nav.Link as={Link} to="/brukervilkår" className="mt-2">
-            Brukervilkår
-          </Nav.Link>
+        <li className="d-flex flex-column">
+            <p><span className="fw-bold">Tlf:</span> +47 941 73 799</p>
+            <p><span className="fw-bold">Epost:</span> andersen.elektromekaniske<br className="d-sm-none"/>@gmail.com</p>
         </li>
       </ul>
-      <div className="d-none d-sm-block d-flex flex-column col-2">
+      <div className="d-none d-sm-block d-flex flex-column col-sm-2 text-end pe-sm-4 pe-lg-5">
         <Nav.Link as={Link} to="/">
           <img
             src={`${process.env.PUBLIC_URL}/images/AEM-logo-2.png`}
@@ -38,6 +32,10 @@ function Footer() {
             id="logo"
           />
         </Nav.Link>
+      </div>
+      <div className="col-12 pt-3">
+        <hr/>
+      <p className=" text-center small fw-light">© 2024 Andersen Elektro Mekaniske. All Rights Reserved</p>
       </div>
     </footer>
   );
